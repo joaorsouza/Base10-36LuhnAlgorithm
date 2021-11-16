f1f6dc9c2918b24f8c493678522ea977e9f2fc30
João Rafael de Souza <joaorsouza@yahoo.com.br>
2021-11-16 07:47:02 -0300

    Add readme file

---
+++ README.md
@@ -0,0 +1,20 @@
+# What is this
+
+Check if your alphanumeric digits is valid using the Luhn Algorithm
+
+# Installation and usage
+
+`npm install base10-36luhnalgorithm`
+
+Then...
+
+...
+
+import validateCheckCharacter from 'base10-36luhnalgorithm'
+
+console.log(validateCheckCharacter('3081034158405873'))
+
+...
+
+You can pass only numbers(base 10) or numbers and letters(base 36). The algorithm will check if the digits is valid and return true/false 
+
--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-function greeter(person) {
-    return "Hello, " + person;
-}
-var user = "Jane User";
-document.body.textContent = greeter(user);
+"use strict";
+exports.__esModule = true;
+var codePoints = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var codePointFromCharacter = function (character) {
+    return codePoints.indexOf(character);
+};
+var checkIfStringHasOnlyDigits = function (value) {
+    if (value.match(/^[0-9]+$/) != null) {
+        return true;
+    }
+    return false;
+};
+var validateCheckCharacter = function (input) {
+    var factor = 1;
+    var sum = 0;
+    var n = checkIfStringHasOnlyDigits(input) ? 10 : 36;
+    // Starting from the right, work  leftwards
+    // Now, the initial "factor" will always be "1"
+    // since the last character is the check character.
+    for (var i = input.length - 1; i >= 0; i--) {
+        var codePoint = codePointFromCharacter(input.charAt(i));
+        var addend = factor * codePoint;
+        // Alternate the "factor" that each "codePoint" is multiplied by
+        factor = factor === 2 ? 1 : 2;
+        // Sum the digits of the "addend" as expressed in base "n"
+        addend = Math.floor(addend / n) + (addend % n);
+        sum += addend;
+    }
+    var remainder = sum % n;
+    return remainder === 0;
+};
+exports["default"] = validateCheckCharacter;
